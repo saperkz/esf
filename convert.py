b29119c513f7960d52ed44d7634bc34a1acef49c
saperkz <64010062+saperkz@users.noreply.github.com>
2020-05-11 16:40:14 +0600

    LAST

--- convert.py
+++ convert.py
@@ -14,7 +14,7 @@ passwd = input('Input password: ')
 find_orig_certname_auth=glob.glob("AUTH_*")
 p12_auth=open(*find_orig_certname_auth, "rb") 
 cert_auth = crypto.load_pkcs12(p12_auth.read(),passwd)
-p12_auth.close()   
+p12_auth.close()
 
 exp_auth = open("AUTH.pem", "wb")
 exp_auth.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert_auth.get_certificate()))
@@ -31,7 +31,21 @@ exp_rsa = open("RSA.pem", "wb")
 exp_rsa.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert_rsa.get_certificate()))
 exp_rsa.close()
 
-#=============================================
+#===========USERNAME-CHECK======================
 
+exp_auth = open("AUTH.pem")
+certn_auth = crypto.load_certificate(crypto.FILETYPE_PEM, exp_auth.read())
+subject_auth=certn_auth.get_subject()
+issued_to_auth=subject_auth.CN
+
+exp_rsa = open("RSA.pem")
+certn_rsa = crypto.load_certificate(crypto.FILETYPE_PEM, exp_rsa.read())
+subject_rsa=certn_rsa.get_subject()
+issued_to_rsa=subject_rsa.CN
+
+print('Сертификат AUTH экспортирован для -',issued_to_auth)
+print('Сертификат RSA экспортирован для -',issued_to_rsa)
+
+input()
 
 
