f21b040d79694c4c9d72eecdfa13e2950b6fc01b
saperkz <64010062+saperkz@users.noreply.github.com>
2020-06-03 01:04:41 +0600

    last

--- convertP12_to_PEM.py
+++ convertP12_to_PEM.py
@@ -1,6 +1,6 @@
 # ***************************
 # App: ConverP12
-# Description: Converts all certificates in folder with name "AUTH..." and "RSA..." also shows "issued to"
+# Description: Converts all certificates in folder with name "AUTH..." and "RSA..." also shows "issued to" and expiration date
 # Date: 03.04.2020
 # Author: Olzhas Omarov
 # Python version: 3.8
@@ -8,7 +8,7 @@
 # Email: ooskenesary@gmail.com
 # ***************************
 
-import glob
+import glob, datetime
 from OpenSSL import crypto
 
 
@@ -43,14 +43,27 @@ certn_auth = crypto.load_certificate(crypto.FILETYPE_PEM, exp_auth.read())
 subject_auth=certn_auth.get_subject()
 issued_to_auth=subject_auth.CN
 
+valid_auth= certn_auth.get_notAfter()
+string_auth=valid_auth.decode('UTF-8','ignore')
+date_time_obj = string_auth[0:-1]
+valid_auth_date = datetime.datetime.strptime(date_time_obj, '%Y%m%d%H%M%S')
+valid_auth_until = valid_auth_date.strftime("%d.%m.%Y")
+
+
 exp_rsa = open("RSA.pem")
 certn_rsa = crypto.load_certificate(crypto.FILETYPE_PEM, exp_rsa.read())
 subject_rsa=certn_rsa.get_subject()
 issued_to_rsa=subject_rsa.CN
 
+valid_rsa= certn_rsa.get_notAfter()
+string_rsa=valid_rsa.decode('UTF-8','ignore')
+date_time_obj = string_rsa[0:-1]
+valid_rsa_date = datetime.datetime.strptime(date_time_obj, '%Y%m%d%H%M%S')
+valid_rsa_until = valid_rsa_date.strftime("%d.%m.%Y")
+
+print('\n')
 print('Сертификат AUTH экспортирован для -',issued_to_auth)
+print('Сертификат AUTH действителен до -',valid_auth_until,'\n')
 print('Сертификат RSA экспортирован для -',issued_to_rsa)
-
+print('Сертификат RSA действителен до -',valid_rsa_until)
 input()
-
-
